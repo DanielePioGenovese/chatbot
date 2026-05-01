dd2e16581a87cc19dae74bab458f1965e75fec94
DanielePioGenovese <delitedaniele@gmail.com>
2026-05-01 21:50:54 +0200

    Test ai working correctly

--- src/model.py
+++ src/model.py
@@ -1,9 +1,9 @@
 from llama_cpp import Llama
 
-def generate_response(prompt, docs):
+def generate_response(prompt, docs, llm):
     full_prompt = f'Use this context: {docs} to answer to this prompt: {prompt}'
 
-    response = Llama.create_chat_completion(
+    response = llm.create_chat_completion(
         messages=[
             {
                 "role":"system",
@@ -13,13 +13,13 @@ def generate_response(prompt, docs):
         ],
         temperature=0.1
     )
-    return response["choises"][0]["message"]["content"]
-
+    return response["choices"][0]["message"]["content"]
 
 llm = Llama(
-    model_path="",
+    model_path="models/llama-3.2-3b-instruct-q8_0.gguf",
     n_gpu_layers=-1,
     n_ctx=8192,
     verbose=False
 )
 
+print(generate_response("What do you think about Apples?","Are a type of fruit",llm=llm))
