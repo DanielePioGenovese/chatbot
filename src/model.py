6dc95c504aa4fae6bd7f5b80b4b5d30bdd13ef6c
DanielePioGenovese <delitedaniele@gmail.com>
2026-05-02 09:16:02 +0200

    VLLM
    Trying Vllm, not LlamaCPP

--- src/model.py
+++ src/model.py
@@ -1,25 +1,9 @@
-from llama_cpp import Llama
+from vllm import LLM
 
-def generate_response(prompt, docs, llm):
-    full_prompt = f'Use this context: {docs} to answer to this prompt: {prompt}'
+llm = LLM(model="facebook/opt-125m")
+outputs = llm.generate("Hello, my name is")
 
-    response = llm.create_chat_completion(
-        messages=[
-            {
-                "role":"system",
-                "content":"You are an expert helpful assistant to answer questions about a company"
-            },
-            {"role":"user","content":full_prompt}
-        ],
-        temperature=0.1
-    )
-    return response["choices"][0]["message"]["content"]
-
-llm = Llama(
-    model_path="models/llama-3.2-3b-instruct-q8_0.gguf",
-    n_gpu_layers=-1,
-    n_ctx=8192,
-    verbose=False
-)
-
-print(generate_response("What do you think about Apples?","Are a type of fruit",llm=llm))
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
