dfdb395440b5c7a16766251c76fd3f46c55faddc
DanielePioGenovese <delitedaniele@gmail.com>
2026-05-02 09:44:39 +0200

    Working with VLLM as a server

--- src/main.py
+++ src/main.py
@@ -1,1 +1,25 @@
+from pydantic_ai import Agent
+import asyncio
+
+agent = Agent(
+    model="meta-llama/Llama-3.2-3B-Instruct",
+    base_url='http://localhost:8000/v1',
+    api_key='vllm-is-free'
+)
+
+async def main():
+
+    result = await agent.run('What is the capital of italy?')
+    print(result.output)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+# outputs = llm.generate("Hello, my name is")
+
+# for output in outputs:
+#     prompt = output.prompt
+#     generated_text = output.outputs[0].text
+#     print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
 
--- src/model.py
+++
@@ -1,9 +0,0 @@
-from vllm import LLM
-
-llm = LLM(model="facebook/opt-125m")
-outputs = llm.generate("Hello, my name is")
-
-for output in outputs:
-    prompt = output.prompt
-    generated_text = output.outputs[0].text
-    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
