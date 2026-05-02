b60774f74c8b6edb7d0a2c06e652dc8f4e80b4cc
DanielePioGenovese <delitedaniele@gmail.com>
2026-05-02 16:33:31 +0200

    Qdrant three model bases

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,22 +1,37 @@
 from dataclasses import dataclass
 from qdrant_client import QdrantClient
-from pydantic import BaseModel
+from qdrant_client.models import Distance, VectorParams, models, Document, PointStruct
 
 client = QdrantClient('http://localhost:6333')
 
 collection_name = 'small_metal_parts'
 
-if not client.collection_exists():
-    client.create_collection(
-        collection_name=collection_name,
-        vectors_config=client.get_fastembed_vector_params()
-    )
+dense_embedding_model = 'BAAI/bge-m3'
+sparse_embedding_model = 'Qdrant/bm25'
+late_interaction_embedding_model = "answerdotai/answerai-colbert-small-v1"
 
-class Documents(BaseModel):
-    id : int
-    text : str
-    metadata : dict
 
-docs = [
-    Documents()
-]
+if client.collection_exists(collection_name=collection_name):
+    client.delete_collection(collection_name=collection_name)
+
+client.create_collection(
+    collection_name,
+    vectors_config={
+        "dense": models.VectorParams(
+            size=1024,
+            distance=models.Distance.COSINE
+        ),
+        "multi": models.VectorParams(
+            size=96,
+            distance=models.Distance.COSINE,
+            multivector_config=models.MultiVectorConfig(
+                comparator=models.MultiVectorComparator.MAX_SIM
+            ),
+            hnsw_config=models.HnswConfigDiff(m=0)
+        ),
+    },
+    sparse_vectors_config={
+        "sparse":models.SparseVectorParams(modifier=models.Modifier.IDF)
+    }
+)
+
