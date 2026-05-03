720e3c7f60a32473bcfb1c1b6d43cfc36dc5840f
DanielePioGenovese <delitedaniele@gmail.com>
2026-05-03 12:12:32 +0200

    Moving the entire work for the RAG on the server

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,15 +1,34 @@
 from dataclasses import dataclass
 from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams, models, Document, PointStruct
+from pathlib import Path
+from fastembed import TextEmbedding, SparseTextEmbedding, LateInteractionTextEmbedding
+
+import requests
+
+base_url = "http://localhost:6333"
+
+models_to_setup = [
+    {"name": "BAAI/bge-small-en-v1.5", "type": "text"},
+    {"name": "Qdrant/bm25", "type": "sparse"},
+    {"name": "colbert-ir/colbertv2.0", "type": "text"} 
+]
+
+for model in models_to_setup:
+    response = requests.post(f"{base_url}/collections/inference/models", json=model)
+    print(f"Setup {model['name']}: {response.json()}")
 
 client = QdrantClient('http://localhost:6333')
 
 collection_name = 'small_metal_parts'
 
-dense_embedding_model = 'BAAI/bge-m3'
+dense_embedding_model = 'BAAI/bge-base-en'
 sparse_embedding_model = 'Qdrant/bm25'
-late_interaction_embedding_model = "answerdotai/answerai-colbert-small-v1"
+late_interaction_embedding_model = "colbert-ir/colbertv2.0"
 
+dense_model = TextEmbedding("BAAI/bge-small-en-v1.5")
+sparse_model = SparseTextEmbedding("Qdrant/bm25")
+multi_model = LateInteractionTextEmbedding("colbert-ir/colbertv2.0")
 
 if client.collection_exists(collection_name=collection_name):
     client.delete_collection(collection_name=collection_name)
@@ -18,7 +37,7 @@ client.create_collection(
     collection_name,
     vectors_config={
         "dense": models.VectorParams(
-            size=1024,
+            size=768,
             distance=models.Distance.COSINE
         ),
         "multi": models.VectorParams(
@@ -35,3 +54,36 @@ client.create_collection(
     }
 )
 
+def get_points(directory_path):
+    path = Path(directory_path)
+    
+    print(f"Reading: {path.absolute()}")
+
+    for idx, file in enumerate(path.iterdir()):
+        if not file.is_file():
+            continue
+            
+        content = file.read_text(encoding="utf-8")
+
+        print("FILE CONTENT:")
+
+        print(content)
+        
+        yield PointStruct(
+            id=idx,
+            vector={
+                "dense": Document(text=content, model=dense_embedding_model),
+                "sparse": Document(text=content, model=sparse_embedding_model),
+                "multi": Document(text=content, model=late_interaction_embedding_model),
+            },
+            payload={
+                "title": file.name, 
+                "description": content
+            }
+        )
+
+client.upload_points(
+    collection_name=collection_name,
+    points=get_points('docs'),
+    batch_size=25
+)
