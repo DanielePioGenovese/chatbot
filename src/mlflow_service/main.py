93a333c78eb31d2ca526809105e5f29bed474ea8
DanielePioGenovese <delitedaniele@gmail.com>
2026-05-05 21:09:44 +0200

    Working to implement MlFlow as a service

--- src/mlflow_service/main.py
+++ src/mlflow_service/main.py
@@ -7,24 +7,25 @@ logger = logging.getLogger(__name__)
 
 s = Settings()
 
-mlflow.set_tracking_uri('http://mlflow:5000')
+mlflow.set_tracking_uri('http://localhost:5000')
 mlflow.set_experiment('chatbot')
 
 with mlflow.start_run():
 
-    # tokenizer = AutoTokenizer.from_pretrained(s.model_id)
-    # model = AutoModelForCausalLM.from_pretrained(
-    #     s.model_id,
-    #     device_map="auto",
-    #     torch_dtype="auto"
-    # )
+    tokenizer = AutoTokenizer.from_pretrained(s.model_id)
 
-    # mlflow.transformers.log_model(
-    #     transformers_model=model,
-    #     artifact_path="/models",
-    #     input_example="Hi, how can i help you with?",
-    #     metadata={"quantization": "GPTQ-Int4", "base_model": "Qwen2.5-7B"}
-    # )
+    components = {
+        "model": s.model_id,
+        "tokenizer": tokenizer,
+    }
+
+    mlflow.transformers.log_model(
+        transformers_model=components,
+        artifact_path="model_artifacts",
+        task="text-generation",
+        input_example="Hi, how can i help you with?",
+        metadata={"quantization": "GPTQ-Int4", "base_model": "Qwen2.5-7B"}
+    )
 
     prompt = mlflow.genai.register_prompt(
         name=s.prompt_name,
