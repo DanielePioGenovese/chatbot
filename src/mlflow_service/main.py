a56b6548f7917934f175435996f235776308e78a
Daniele <d.genovese@mynext.it>
2026-05-07 16:41:33 +0200

    Working with the MLFlow server

--- src/mlflow_service/main.py
+++ src/mlflow_service/main.py
@@ -1,5 +1,4 @@
 import mlflow 
-from transformers import AutoTokenizer
 from settings import Settings
 import logging
 
@@ -12,24 +11,12 @@ mlflow.set_experiment('chatbot')
 
 with mlflow.start_run():
 
-    tokenizer = AutoTokenizer.from_pretrained(s.model_id)
-
-    components = {
-        "model": s.model_id,
-        "tokenizer": tokenizer,
-    }
-
     mlflow.transformers.log_model(
-        transformers_model=components,
+        transformers_model=s.model_id, 
         artifact_path="model_artifacts",
         task="text-generation",
         input_example="Hi, how can i help you with?",
         metadata={"quantization": "GPTQ-Int4", "base_model": "Qwen2.5-7B"}
     )
-
-    prompt = mlflow.genai.register_prompt(
-        name=s.prompt_name,
-        template=s.initial_template,
-    )
-
-    logger.info(f'Model: and Prompt: {prompt.name} saved succesfully!')
+    
+    logger.info('Model saved succesfully')
