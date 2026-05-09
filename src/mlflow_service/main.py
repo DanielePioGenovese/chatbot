f359b19b6e8ac998ea2f1cc5c9239d00182fec48
DanielePioGenovese <delitedaniele@gmail.com>
2026-05-09 10:58:42 +0200

    Working with mlflow
    GOAL: Smaller Vllm and qdrant images, better scalability and reproducibility

--- src/mlflow_service/main.py
+++ src/mlflow_service/main.py
@@ -1,22 +1,28 @@
 import mlflow 
 from settings import Settings
+from transformers import AutoModelForMaskedLM, AutoTokenizer
 import logging
 
 logger = logging.getLogger(__name__)
 
 s = Settings()
 
-mlflow.set_tracking_uri('https://mlflow-server:5000')
-mlflow.set_experiment('chatbot')
+mlflow.set_tracking_uri(s.mlflow_url)
+mlflow.set_experiment(s.set_experiment)
+
+tokenizer = AutoTokenizer.from_pretrained(s.model_id)
+model = AutoModelForMaskedLM.from_pretrained(s.model_id)
 
 with mlflow.start_run():
 
+    components = {
+        'model' : model,
+        'tokenizer' : tokenizer,
+    }
+
     mlflow.transformers.log_model(
-        transformers_model=s.model_id, 
-        artifact_path="model_artifacts",
-        task="text-generation",
-        input_example="Hi, how can i help you with?",
-        metadata={"quantization": "GPTQ-Int4", "base_model": "Qwen2.5-7B"}
+        transformers_model=components, 
+        name="Main_model"
     )
     
     logger.info('Model saved succesfully')
--- src/mlflow_service/settings.py
+++ src/mlflow_service/settings.py
@@ -5,3 +5,6 @@ class Settings(BaseSettings):
 
     initial_template : str = "You are an helpful assitant for a small metal parts industry"
     prompt_name : str = "rag-assistant"
+
+    mlflow_url : str = 'http://mlflow-server:5000'
+    set_experiment : str = "chatbot"
