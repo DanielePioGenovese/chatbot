92c9e80848d707d19946b7f78ca9e5ae208c30d2
DanielePioGenovese <delitedaniele@gmail.com>
2026-05-10 21:06:36 +0200

    Mlflow working well
    Improving the agent code

---
+++ src/agent/agent.py
@@ -0,0 +1,46 @@
+import asyncio
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_agent
+from settings import Settings
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+s = Settings()
+
+
+client = MultiServerMCPClient(
+    {
+        "rag" : {
+            "transport" : s.transport_settings,
+            "url" : s.uri_mcp_server
+        }
+    }
+)
+
+def get_agent():
+    
+    tools = client.get_tools()
+    logger.info("MCP Tool loaded correctly!")
+    
+    llm = ChatOpenAI(
+        model=s.model,
+        base_url=s.uri_vllm,
+        api_key='EMPTY',
+        temperature=s.temperature,
+        timeout=s.timeout,
+        streaming=s.streaming
+    )
+
+    agent = create_agent(
+        model=llm,
+        tools=tools,
+        prompt=s.mflow_prompt_name
+    )   
+
+    return agent
--- src/agent/main.py
+++ src/agent/main.py
@@ -1,17 +1,22 @@
-import asyncio
-from langchain_mcp_adapters.client import MultiServerMCPClient
-from langchain_openai import ChatOpenAI
-from langchain.agents import create_agent
-from settings import Settings
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import mlflow
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
+from settings import Settings
+import logging
+from agent import get_agent
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
 app = FastAPI()
 s = Settings()
+
 limiter = Limiter(key_func=get_remote_address)
 app.state.limiter = Limiter()
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
@@ -25,41 +30,9 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-client = MultiServerMCPClient(
-    {
-        "rag" : {
-            "transport" : s.transport_settings,
-            "url" : s.uri_mcp_server
-        }
-    }
-)
-
 prompt = mlflow.genai.load_prompt(s.mflow_prompt_name)
-
-tools = client.get_tools()
-
-# I will add the correct prompt format here
-
-llm = ChatOpenAI(
-    model=s.model,
-    base_url=s.uri_vllm,
-    api_key='EMPTY',
-    temperature=s.temperature,
-    timeout=s.timeout,
-    streaming=s.streaming
-)
-
-agent = create_agent(
-    model=llm,
-    tools=tools,
-    prompt=s.mflow_prompt_name #Necessity to take the prompt from MLFLOW, this strategy is wrong
-)   
 
 @app.get("/agent/{prompt}")
 @limiter.limit("10/minute")
 async def agent_answer(prompt : str):
     return await agent.invoke(prompt)
-
-
-if __name__ == '__main__':
-    asyncio.run(agent_answer())
