655e1399e3c60b9376c3b2e43b4ecd695e3f079f
daliaabbruciati <daliaabbruciati@gmail.com>
2026-05-26 00:14:45 +0200

    Added menu routes and UI improvement for the homepage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,36 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./routes/App.jsx";
+import App, {
+  CompanyPage,
+  HomePage,
+  ProductsPage,
+  QualityControlPage,
+} from "./routes/App.jsx";
 import "./styles.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "products",
+        element: <ProductsPage />,
+      },
+      {
+        path: "company",
+        element: <CompanyPage />,
+      },
+      {
+        path: "quality-control",
+        element: <QualityControlPage />,
+      },
+    ],
   },
 ]);
 
